a163dbe80f458df644aa78c4bf723a4ccc135966
imeekwere <ime.ekwere@e4email.net>
2021-09-25 12:47:55 +0100

    feat: add folder structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar
+ = () => {
+    return ( 
+        <div className="nav">
+            <div>
+                <p>Dashboard</p>
+            </div>
+            <div>
+                <p>icon and image</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar
+;
