0e69d7ec32c9e0bc367b1697ffe1f59f7e31ab84
imeekwere <ime.ekwere@e4email.net>
2021-09-26 15:34:10 +0100

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { useLocation, useParams } from "react-router";
+import Image from 'react-bootstrap/Image'
 
 const Navbar
  = () => {
@@ -12,7 +13,8 @@ const Navbar
                 <p> { location.pathname.replace('/', '') ? location.pathname.replace('/', '') : 'Dashboard' } </p>
             </div>
             <div>
-                <p>icon and image</p>
+            <i class='bx bx-bell'></i>
+            <Image src="https://lh3.googleusercontent.com/ogw/ADea4I4SxN_qQ1a5kbTRn4g5fDx-xfmVkv_eE4ehe2mH=s32-c-mo" roundedCircle />
             </div>
         </div>
      );
