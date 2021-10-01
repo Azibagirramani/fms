1e937094ba4ac08c9d057d67f93f26eef87b7adf
Kelvin Mansi <kelvinmansi2@gmail.com>
2021-10-01 13:51:20 +0100

    project page

---
+++ src/components/Milestones.js
@@ -0,0 +1,149 @@
+import { Fragment } from "react";
+function Milestones() {
+  return (
+    <Fragment>
+      <div className="">
+        <div className="d-flex px-5 mt-4 flex-column">
+          <div className="d-flex m-0 p-0 justify-content-between align-items-center">
+            <div className="d-flex gap-3 border-start border-primary border-5">
+              <i class="bx bxs-check-square ms-2 fs-3 icon-color-plus"></i>
+              <div className="d-flex flex-column">
+                <p className="m-0 p-0 fw-bold">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                  quaerat asperiores
+                </p>
+                <p>
+                  <span className="icon-color-plus">Completed</span> 1/23/2021 -
+                  1/23/2021
+                </p>
+              </div>
+            </div>
+            <i
+              className="bx button bxs-plus-square ms-2 fs-3 icon-color-plus"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            ></i>
+          </div>
+          <div className="ps-5 mt-4 d-flex justify-content-between flex-column flex-lg-row">
+            <i className="bx bx-circle text-secondary fs-3"></i>
+            <div className="flex-fill ms-1">
+              <p className="fw-bold">
+                Lore nter beyreacat <span className="fw-light">1/23/2021</span>
+              </p>
+              <p className="fw-bold">N1000.000</p>
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                quaerat asperiores nulla alias impedit quod debitis suscipit
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                quaerat asperiores nulla alias impedit quod debitis suscipit
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                quaerat asperiores nulla alias impedit quod debitis suscipit
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                quaerat asperiores nulla alias impedit quod debitis suscipit
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                quaerat asperiores nulla alias impedit quod debitis suscipit
+              </p>
+              <div>
+                <div className="d-flex">
+                  Download:{" "}
+                  <span className="d-flex">
+                    <i class="bx bxs-file-doc text-danger fs-3"></i>
+                    <i class="bx bxs-file-pdf text-secondary fs-3"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex gap-4">
+              <i class="bx bx-trash text-danger fs-3"></i>
+              <i class="bx bx-edit text-primary fs-3"></i>
+            </div>
+          </div>
+        </div>
+        <div className="py-4 border-top mt-5 px-5">
+          <div className="d-flex justify-content-between align-items-center border-5 gap-2 border-start border-secondary">
+            <div className="d-flex align-items-center">
+              <i className="bx bxs-square ms-2 fs-3"></i>
+              <p className="mt-3 ms-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit{" "}
+              </p>
+            </div>
+            <i className="bx bxs-plus-circle ms-2 fs-3 icon-color-plus"></i>
+          </div>
+        </div>
+        <div className="card-footer d-flex justify-content-between bg-white py-3 px-5">
+          <div className="d-flex gap-5">
+            <button className="btn btn-primary rounded-pill px-4 fw-bold">
+              Suspend Project
+            </button>
+            <button className="btn btn-danger rounded-pill px-4 fw-bold">
+              Stop Project
+            </button>
+          </div>
+          <button className="btn btn-primary rounded-pill px-4 fw-bold">
+            Save and Continue
+          </button>
+        </div>
+      </div>
+
+      {/* popup modal  */}
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+      >
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Milestone Report
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <p>
+                Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit
+              </p>
+              <p>Date: 1/12/2021</p>
+              <form>
+                <div className="row">
+                  <div className="col-md-3 fw-bold">Amount Spent:</div>
+                  <div className="col-md-7">
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div className="col-md-2">
+                    <select class="form-select rounded">
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+                <div>
+                  <label for="description" class="form-label">description</label>
+                  <textarea  className="form-control w-100"/>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">
+                    Save changes
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Milestones;
--- src/views/Funds.js
+++ src/views/Funds.js
@@ -1,13 +1,9 @@
-const Funds
- = () => {
-    return ( 
-        <div className="mt-5">
-            <h1>
-                Funds
-            </h1>
-        </div>
-     );
-}
- 
-export default Funds
-;
+const Funds = () => {
+  return (
+    <div className="mt-5">
+      <h1>Funds</h1>
+    </div>
+  );
+};
+
+export default Funds;
