4bdc49e2365177925427bdecd4cbeaaee38d484f
Kelvin Mansi <kelvinmansi2@gmail.com>
2021-10-05 18:55:51 +0100

    dashboard ui

--- src/App.js
+++ src/App.js
@@ -2,41 +2,18 @@ import "./vendor/custom.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div class="d-flex flex-column flex-shrink-0 p-3 vh-100 nav-base">
-      <div className="border-bottom">
-        <i className="bx bx-circle fs-1 mb-2"> </i>
+    <>
+      <div className="mt-5 container border d-flex justify-content-between">
+        <p className="fs-6 fw-bold">Dashboard</p>
+        <div className="d-flex  gap-1">
+          <i className="bx bx-bell text-secondary fs-3 position-relative">
+            <span class="position-absolute top-0 start-0 translate-middle badge bg-danger">
+              <span class="visually-hidden">unread messages</span>
+            </span>
+          </i>
+        </div>
       </div>
-      <ul class="nav nav-pills fw-bold  flex-column mb-auto">
-        <li>
-          <a href="#/" className="nav-link d-flex align-content-center">
-            <i className="bx bxs-dashboard fs-3 me-2"></i>
-            <span className="fs-6 mt-1">Dashboard</span>
-          </a>
-        </li>
-        <li>
-          <a href="#/" className="nav-link d-flex align-content-center">
-            <i className="bx bxs-wallet-alt fs-3 me-2"></i>
-            <span className="fs-6 mt-1">Funds</span>
-          </a>
-        </li>
-        <li>
-          <a href="#/" className="nav-link d-flex align-content-center">
-            <i className="bx bx-archive fs-3 me-2"></i>
-            <span className="fs-6 mt-1">Projects</span>
-          </a>
-        </li>
-        <li>
-          <a href="#/" className="nav-link d-flex align-content-center">
-            <i className="bx bx-abacus fs-3 me-2"></i>
-            <span className="fs-6 mt-1">Settings</span>
-          </a>
-        </li>
-      </ul>
-      <div className="d-flex align-content-center gap-3 border-top">
-        <i className="bx bx-log-out fs-4 icon-color mt-3"></i>
-        <span className="icon-color fs-6 mt-3">Logout</span>
-      </div>
-    </div>
+    </>
   );
 }
 export default App;
---
+++ src/components/SideBar.js
@@ -0,0 +1,35 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 vh-100 nav-base">
+<div className="border-bottom">
+  <i className="bx bx-circle fs-1 mb-2"> </i>
+</div>
+<ul class="nav nav-pills fw-bold  flex-column mb-auto">
+  <li>
+    <a href="#/" className="nav-link d-flex align-content-center">
+      <i className="bx bxs-dashboard fs-3 me-2"></i>
+      <span className="fs-6 mt-1">Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#/" className="nav-link d-flex align-content-center">
+      <i className="bx bxs-wallet-alt fs-3 me-2"></i>
+      <span className="fs-6 mt-1">Funds</span>
+    </a>
+  </li>
+  <li>
+    <a href="#/" className="nav-link d-flex align-content-center">
+      <i className="bx bx-archive fs-3 me-2"></i>
+      <span className="fs-6 mt-1">Projects</span>
+    </a>
+  </li>
+  <li>
+    <a href="#/" className="nav-link d-flex align-content-center">
+      <i className="bx bx-abacus fs-3 me-2"></i>
+      <span className="fs-6 mt-1">Settings</span>
+    </a>
+  </li>
+</ul>
+<div className="d-flex align-content-center gap-3 border-top">
+  <i className="bx bx-log-out fs-4 icon-color mt-3"></i>
+  <span className="icon-color fs-6 mt-3">Logout</span>
+</div>
+</div>
